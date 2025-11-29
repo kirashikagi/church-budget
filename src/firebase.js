23a498be7dff8acd80ef56ce0ac17ac6457864dd
kirashikagi <kirareba2022@gmail.com>
2025-11-29 18:47:28 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
+// ВНИМАНИЕ: СЮДА ВСТАВЬТЕ СВОИ КЛЮЧИ ВМЕСТО МОИХ ЗАГЛУШЕК
 const firebaseConfig = {
   apiKey: "AIzaSyCy6qvyrhRxap8A78TDG9MbvdOCPgbOGCM",
   authDomain: "churchbudget-620e2.firebaseapp.com",
@@ -16,9 +11,6 @@ const firebaseConfig = {
   messagingSenderId: "238428969794",
   appId: "1:238428969794:web:59f92b2901882d06b006db"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
